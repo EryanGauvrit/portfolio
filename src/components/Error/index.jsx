e055c1135f678f0d1e15c6c586448b49258c9dc9
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-02-23 17:35:15 +0100

    add error page & style

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,16 @@
+import { Form, Link } from "react-router-dom";
+import errorPicture from "../../assets/picture-error.jpg"
+
 function Error() {
     return (
-        <div>
-            Error
-        </div>
+        <main className="error-main">
+            <section>
+                <h2>ERREUR !</h2>
+                <p>Cette adresse ne mène nulle part ...</p>
+                <img src={errorPicture} alt='Erreur' />
+                <Link to="/">Retour page d'accueil</Link>
+            </section>
+        </main>
     )
 }
 
