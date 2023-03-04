c9447500f1bf72b45d243b94185e6b8c51b3536e
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2023-03-04 15:43:24 +0100

    Add pp in Home page

--- src/components/ContentHome/index.jsx
+++ src/components/ContentHome/index.jsx
@@ -1,9 +1,9 @@
 
-function ContentHome({ title, content, link }) {
+function ContentHome({ title, content, link, pp }) {
 
     return (
         <article>
-            <h2>{title}</h2>
+            <h2>{title} {pp}</h2>
             <p>{content}</p>
             {link}
         </article>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import BannerHome from "../../components/BannerHome";
 import ContentHome from "../../components/ContentHome";
 import { Link } from "react-router-dom";
 import ChangeTitlePage from "../../components/ChangeTitlePage";
+import pp from "../../assets/PP.png";
 
 function Home() {
     return (
@@ -13,13 +14,14 @@ function Home() {
             <BannerHome />
             <section className="home-content">
                 <ContentHome
-                    title="Bienvenue sur mon portefolio !"
+                    title="Bienvenue sur mon portfolio !"
                     content="Si vous êtes arrivés jusqu'ici, c'est que soit vous êtes recruteur, soit vous êtes curieux. Je vous laisse découvrir ma création si vous n'êtes pas encore convaincu de ce que je peux faire et ce que je pourrai faire bientôt ..."
                 />
                 <ContentHome
                     title="Qui suis-je ?"
                     content="Rapidement, si vous êtes recruteur, je suis celui que vous attendez : un junior en plein épanouissement devant les infinies possibilités qu'offre le développement web. Si vous êtes simplement curieux ou un confrère, peut-être que ce projet vous donnera des idées pour vos futures créations, du moins je l'espère."
                     link={<p className="link-home-content">Cliquez pour en savoir <Link to='/profil'>plus ...</Link></p>}
+                    pp={<img src={pp} alt="Profil" />}
                 />
             </section>
 
